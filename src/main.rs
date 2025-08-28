4141c800d4ef2c527271d9f7dea59329c72a9f2f
lake <posuihushui@icloud.com>
2025-08-28 15:51:23 +0800

    Fix output for cloned string and clean up vector capacity function

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,22 @@ fn main() {
     let s3 = String::from("hello");
     let s4 = s3.clone();
     println!("{s3}, world!");
+    println!("{s4}, world!");
 
     // 返回值与作用域
+
+    test_vect_capacity();
 }
 
-fn calcute_length(s: String) -> (String, usize) {
-    let length = s.len();
-    (s, length)
+fn test_vect_capacity() {
+    let mut v: Vec<i32> = vec![1, 2, 3];
+    println!("Len: {}, Capacity: {}", v.len(), v.capacity()); // Len: 3, Capacity: 3
+    v.push(4);
+    println!("Len: {}, Capacity: {}", v.len(), v.capacity()); // Len: 4, Capacity: 6
+    v.push(5);
+    println!("Len: {}, Capacity: {}", v.len(), v.capacity()); // Len: 5, Capacity: 6
+    v.push(6);
+    println!("Len: {}, Capacity: {}", v.len(), v.capacity()); // Len: 6, Capacity: 6
+    v.push(7);
+    println!("Len: {}, Capacity: {}", v.len(), v.capacity()); // Len: 7, Capacity: 12
 }
